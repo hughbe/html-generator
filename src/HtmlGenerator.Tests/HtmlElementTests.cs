20d126a36fd5413ec131bce712da1dd8d318c282
Hugh Bellamy <hughbellars@gmail.com>
2016-10-03 14:40:41 +0100

    Don't allow attributes with the same name to be added multiple times
    Fixes #22

--- src/HtmlGenerator.Tests/HtmlElementTests.cs
+++ src/HtmlGenerator.Tests/HtmlElementTests.cs
@@ -58,7 +58,7 @@ namespace HtmlGenerator.Tests
             yield return new object[] { new HtmlObject[] { new HtmlAttribute("Attribute1") } };
             yield return new object[] { new HtmlObject[] { new HtmlElement("h1") } };
             yield return new object[] { new HtmlObject[] { new HtmlAttribute("Attribute1"), new HtmlElement("h1") } };
-            yield return new object[] { new HtmlObject[] { new HtmlAttribute("Attribute1"), new HtmlElement("h1"), new HtmlElement("h1"), new HtmlAttribute("Attribute1") } };
+            yield return new object[] { new HtmlObject[] { new HtmlAttribute("Attribute1"), new HtmlElement("h1"), new HtmlElement("h1"), new HtmlAttribute("Attribute2") } };
         }
 
         [Theory]
@@ -128,39 +128,13 @@ namespace HtmlGenerator.Tests
 
         [Theory]
         [MemberData(nameof(TryGetElement_TestData))]
-        public void TryGetElement(HtmlElement[] elements, string tag, HtmlElement expected)
+        public void TryGetElement(HtmlElement[] elements, string name, HtmlElement expected)
         {
             HtmlElement parent = new HtmlElement("html", elements);
 
             HtmlElement element;
-            Assert.Equal(expected != null, parent.TryGetElement(tag, out element));
+            Assert.Equal(expected != null, parent.TryGetElement(name, out element));
             Assert.Equal(expected, element);
-
-            Assert.Equal(expected != null, parent.HasElement(tag));
-        }
-
-        [Fact]
-        public void TryGetElement_NullName_ThrowsArgumentNullException()
-        {
-            HtmlElement element = new HtmlElement("html");
-            HtmlElement resultElement = null;
-            Assert.Throws<ArgumentNullException>("tag", () => element.TryGetElement(null, out resultElement));
-            Assert.Null(resultElement);
-
-            Assert.Throws<ArgumentNullException>("tag", () => element.HasElement(null));
-        }
-
-        [Theory]
-        [InlineData("")]
-        [InlineData(" \r \t \n")]
-        public void TryGetElement_WhitespaceName_ThrowsArgumentException(string tag)
-        {
-            HtmlElement element = new HtmlElement("html");
-            HtmlElement resultElement = null;
-            Assert.Throws<ArgumentException>("tag", () => element.TryGetElement(tag, out resultElement));
-            Assert.Null(resultElement);
-
-            Assert.Throws<ArgumentException>("tag", () => element.HasElement(tag));
         }
 
         public static IEnumerable<object[]> TryGetAttribute_TestData()
@@ -189,8 +163,6 @@ namespace HtmlGenerator.Tests
             HtmlAttribute attribute;
             Assert.Equal(expected != null, parent.TryGetAttribute(name, out attribute));
             Assert.Equal(expected, attribute);
-
-            Assert.Equal(expected != null, parent.HasAttribute(name));
         }
 
         [Fact]
@@ -200,8 +172,6 @@ namespace HtmlGenerator.Tests
             HtmlAttribute attribute = null;
             Assert.Throws<ArgumentNullException>("name", () => element.TryGetAttribute(null, out attribute));
             Assert.Null(attribute);
-
-            Assert.Throws<ArgumentNullException>("name", () => element.HasAttribute(null));
         }
 
         [Theory]
@@ -211,10 +181,8 @@ namespace HtmlGenerator.Tests
         {
             HtmlElement element = new HtmlElement("html");
             HtmlAttribute attribute = null;
-            Assert.Throws<ArgumentException>("name", () => element.TryGetAttribute(name, out attribute));
+            Assert.Throws<ArgumentNullException>("name", () => element.TryGetAttribute(null, out attribute));
             Assert.Null(attribute);
-
-            Assert.Throws<ArgumentException>("name", () => element.HasAttribute(name));
         }
 
         public static IEnumerable<object[]> Elements_Method_TestData()
@@ -677,17 +645,35 @@ namespace HtmlGenerator.Tests
         [Fact]
         public void Add_DuplicateAttribute_ThrowsInvalidOperationException()
         {
-            HtmlElement attribute = new HtmlElement("html");
-            HtmlAttribute newAttribute = new HtmlAttribute("body");
-            attribute.Add(newAttribute);
+            HtmlElement element = new HtmlElement("html");
+            HtmlAttribute newAttribute = new HtmlAttribute("Attribute");
+            element.Add(newAttribute);
 
-            Assert.Throws<InvalidOperationException>(() => attribute.Add(newAttribute));
-            Assert.Throws<InvalidOperationException>(() => attribute.Add(new HtmlObject[] { newAttribute }));
-            Assert.Throws<InvalidOperationException>(() => attribute.Add((IEnumerable<HtmlObject>)new HtmlObject[] { newAttribute }));
+            Assert.Throws<InvalidOperationException>(() => element.Add(newAttribute));
+            Assert.Throws<InvalidOperationException>(() => element.Add(new HtmlObject[] { newAttribute }));
+            Assert.Throws<InvalidOperationException>(() => element.Add((IEnumerable<HtmlObject>)new HtmlObject[] { newAttribute }));
 
-            Assert.Throws<InvalidOperationException>(() => attribute.AddFirst(newAttribute));
-            Assert.Throws<InvalidOperationException>(() => attribute.AddFirst(new HtmlObject[] { newAttribute }));
-            Assert.Throws<InvalidOperationException>(() => attribute.AddFirst((IEnumerable<HtmlObject>)new HtmlObject[] { newAttribute }));
+            Assert.Throws<InvalidOperationException>(() => element.AddFirst(newAttribute));
+            Assert.Throws<InvalidOperationException>(() => element.AddFirst(new HtmlObject[] { newAttribute }));
+            Assert.Throws<InvalidOperationException>(() => element.AddFirst((IEnumerable<HtmlObject>)new HtmlObject[] { newAttribute }));
+        }
+
+        [Fact]
+        public void Add_EqualAttribute_ThrowsInvalidOperationException()
+        {
+            HtmlElement element = new HtmlElement("html");
+            HtmlAttribute attribute = new HtmlAttribute("Attribute");
+            element.Add(attribute);
+
+            HtmlAttribute newAttribute = new HtmlAttribute(attribute.Name);
+
+            Assert.Throws<InvalidOperationException>(() => element.Add(newAttribute));
+            Assert.Throws<InvalidOperationException>(() => element.Add(new HtmlObject[] { newAttribute }));
+            Assert.Throws<InvalidOperationException>(() => element.Add((IEnumerable<HtmlObject>)new HtmlObject[] { newAttribute }));
+
+            Assert.Throws<InvalidOperationException>(() => element.AddFirst(newAttribute));
+            Assert.Throws<InvalidOperationException>(() => element.AddFirst(new HtmlObject[] { newAttribute }));
+            Assert.Throws<InvalidOperationException>(() => element.AddFirst((IEnumerable<HtmlObject>)new HtmlObject[] { newAttribute }));
         }
 
         [Fact]
