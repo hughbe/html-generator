badf84dd336c1cb5c73b00147b376012b9c82cc0
Hugh Bellamy <hughbellars@gmail.com>
2016-10-03 12:09:02 +0100

    Improve CI testing (#19)

--- src/HtmlGenerator.Tests/Helpers.cs
+++ src/HtmlGenerator.Tests/Helpers.cs
@@ -7,6 +7,7 @@ namespace HtmlGenerator.Tests
     {
         public static void SerializeIgnoringFormatting(HtmlElement element, string expected)
         {
+            expected = expected.Replace("\r\n", "\n").Replace("\n", Environment.NewLine);
             Assert.Equal(expected, element.ToString());
             Assert.Equal(expected, element.Serialize());
             Assert.Equal(expected.Replace(Environment.NewLine, ""), element.Serialize(HtmlSerializeOptions.NoFormatting));
