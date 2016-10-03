341c4b4589c747d2e494e3433f6ee87c1fecfab8
Hugh Bellamy <hughbellars@gmail.com>
2016-10-03 09:42:22 +0100

    Add HtmlElement.AddFirst()
    Fixes #10

--- src/HtmlGenerator/HtmlElement.cs
+++ src/HtmlGenerator/HtmlElement.cs
@@ -59,12 +59,46 @@ namespace HtmlGenerator
                 AddAttribute((HtmlAttribute)content);
             }
         }
+        
+        public void AddFirst(params HtmlObject[] content) => AddFirst((IEnumerable<HtmlObject>)content);
+
+        public void AddFirst(IEnumerable<HtmlObject> content)
+        {
+            Requires.NotNull(content, nameof(content));
+            foreach (HtmlObject obj in content)
+            {
+                AddFirst(obj);
+            }
+        }
 
         private void AddAttribute(HtmlAttribute attribute)
         {
             attribute.RemoveFromParent();
             attribute.Parent = this;
             _attributes.AddLast(attribute);
+        }
+
+        public void AddFirst(HtmlObject content)
+        {
+            Requires.NotNull(content, nameof(content));
+            if (content.Parent == this)
+            {
+                throw new InvalidOperationException("Cannot have a duplicate element or attribute");
+            }
+
+            if (content.ObjectType == HtmlObjectType.Element)
+            {
+                if (content == this)
+                {
+                    throw new InvalidOperationException("Cannot add an object as a child to itself.");
+                }
+                ThrowIfVoid();
+                AddElementFirst((HtmlElement)content);
+            }
+            else
+            {
+                AddAttributeFirst((HtmlAttribute)content);
+            }
         }
 
         public void Add(params HtmlObject[] content) => Add((IEnumerable<HtmlObject>)content);
@@ -76,6 +110,20 @@ namespace HtmlGenerator
             {
                 Add(obj);
             }
+        }
+
+        private void AddElementFirst(HtmlElement element)
+        {
+            element.RemoveFromParent();
+            element.Parent = this;
+            _elements.AddFirst(element);
+        }
+
+        private void AddAttributeFirst(HtmlAttribute attribute)
+        {
+            attribute.RemoveFromParent();
+            attribute.Parent = this;
+            _attributes.AddFirst(attribute);
         }
 
         public void ReplaceAll(params HtmlObject[] content) => ReplaceAll((IEnumerable<HtmlObject>)content);
