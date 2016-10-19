d5d5bf40f661f7f311bce8ddd1330eb253234f9d
Hugh Bellamy <hughbellars@gmail.com>
2016-10-19 12:42:12 +0100

    Add doctype support (#45)
    * Attempt to infer DocTypeType from Doctype
    
    * Implement Equals and ObjectType on HtmlDocument
    
    * Add support for parsing Doctypes

--- src/HtmlGenerator/HtmlDocument.cs
+++ src/HtmlGenerator/HtmlDocument.cs
@@ -1,22 +1,82 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace HtmlGenerator
 {
-    public class HtmlDocument : HtmlElement
+    public class HtmlDocument : HtmlElement, IEquatable<HtmlDocument>
     {
-        public HtmlDocument() : base("html")
-        {
-            Head = new HtmlElement("head");
-            Body = new HtmlElement("body");
+        public HtmlDocument() : base("html") { }
 
-            Add(Head);
-            Add(Body);
+        private HtmlElement _head = null;
+        public HtmlElement Head
+        {
+            get { return _head; }
+            set
+            {
+                _head?.RemoveFromParent();
+                if (value != null)
+                {
+                    Add(value);
+                }
+                _head = value;
+            }
         }
-        
-        public HtmlElement Head { get; set; }
-        public HtmlElement Body { get; set; }
+
+        public HtmlDocument AddHead()
+        {
+            if (Head != null)
+            {
+                throw new InvalidOperationException("Document already has a head element.");
+            }
+            Head = new HtmlHeadElement();
+            return this;
+        }
+
+        private HtmlElement _body = null;
+        public HtmlElement Body
+        {
+            get { return _body; }
+            set
+            {
+                _body?.RemoveFromParent();
+                if (value != null)
+                {
+                    Add(value);
+                }
+                _body = value;
+            }
+        }
+
+        public HtmlDocument AddBody()
+        {
+            if (Body != null)
+            {
+                throw new InvalidOperationException("Document already has a body element.");
+            }
+            Body = new HtmlBodyElement();
+            return this;
+        }
 
         public HtmlDoctype Doctype { get; set; } = new HtmlDoctype(HtmlDoctypeType.Html5);
+
+        public override HtmlObjectType ObjectType => HtmlObjectType.Document;
+
+        public override bool Equals(object obj) => Equals(obj as HtmlDocument);
+
+        public bool Equals(HtmlDocument other)
+        {
+            if (!base.Equals(other))
+            {
+                return false;
+            }
+            if (Doctype == null)
+            {
+                return other.Doctype == null;
+            }
+            return Doctype.Equals(other.Doctype);
+        }
+
+        public override int GetHashCode() => Doctype == null ? base.GetHashCode() : (base.GetHashCode() ^ Doctype.GetHashCode());
 
         internal override void Serialize(StringBuilder builder, HtmlSerializeOptions serializeType)
         {
@@ -29,6 +89,33 @@ namespace HtmlGenerator
                 }
             }
             base.Serialize(builder, serializeType);
+        }
+
+        public static new HtmlDocument Parse(string text)
+        {
+            HtmlDocument document;
+            if (!TryParse(text, out document))
+            {
+                return null;
+            }
+            return document;
+        }
+
+        public static bool TryParse(string text, out HtmlDocument document)
+        {
+            document = null;
+            if (text == null || text.Length == 0)
+            {
+                return false;
+            }
+
+            Parser parser = new Parser(text, isDocument: true);
+            if (parser.Parse())
+            {
+                document = (HtmlDocument)parser.rootElement;
+                return true;
+            }
+            return false;
         }
     }
 }
